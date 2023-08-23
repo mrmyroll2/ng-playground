7a9fb8269874014755a6cd27655b73826592b536
amierul basyar <amieru.basyar@pmcare.com.my>
2023-08-23 13:25:54 +0800

    Base 1

--- date-logic/src/app/app.component.ts
+++ date-logic/src/app/app.component.ts
@@ -1,10 +1,155 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [
+    `
+      select {
+        font-size: 1.2em;
+        padding: 0.5em;
+        margin: 0.5em;
+      }
+    `,
+  ],
+  template: `
+    <h1>Angular Date Picker</h1>
+
+    <p>Min date: {{ minDate | date : 'yyyy MM dd' }}</p>
+    <p>Max date: {{ maxDate | date : 'yyyy MM dd' }}</p>
+    <p>
+      Selected date : {{ selectedYear }}-{{ selectedMonth }}-{{ selectedDay }}
+    </p>
+
+    <select [(ngModel)]="selectedYear" (ngModelChange)="updateMonths()">
+      <option *ngFor="let year of years" [value]="year">{{ year }}</option>
+    </select>
+
+    <select [(ngModel)]="selectedMonth" (ngModelChange)="updateDays()">
+      <option *ngFor="let month of months" [value]="month">{{ month }}</option>
+    </select>
+
+    <select [(ngModel)]="selectedDay">
+      <option *ngFor="let day of days" [value]="day">{{ day }}</option>
+    </select>
+  `,
 })
-export class AppComponent {
-  title = 'date-logic';
+export class AppComponent implements OnInit {
+  minDate: Date = new Date(2020, 2 - 1, 6);
+  // maxDate: Date = new Date(2025, 11 - 1, 15);
+  maxDate: Date = new Date(2021, 3 - 1, 8);
+
+  selectedYear!: number;
+  selectedMonth!: number;
+  selectedDay!: number;
+
+  years: number[] = [];
+  months: number[] = [];
+  days: number[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.populateYears();
+    this.selectedYear = this.minDate.getFullYear();
+    this.updateMonths();
+  }
+
+  populateYears() {
+    for (
+      let year = this.minDate.getFullYear();
+      year <= this.maxDate.getFullYear();
+      year++
+    ) {
+      this.years.push(year);
+    }
+  }
+
+  updateMonths() {
+    const startMonth =
+      this.selectedYear == this.minDate.getFullYear()
+        ? this.minDate.getMonth() + 1
+        : 1;
+    const endMonth =
+      this.selectedYear == this.maxDate.getFullYear()
+        ? this.maxDate.getMonth() + 1
+        : 12;
+
+    this.months = [];
+    for (let month = startMonth; month <= endMonth; month++) {
+      this.months.push(month);
+    }
+
+    if (this.selectedMonth == null) this.selectedMonth = this.months[0];
+    if (this.selectedMonth < startMonth) {
+      this.selectedMonth = startMonth;
+    } else if (this.selectedMonth > endMonth) {
+      this.selectedMonth = endMonth;
+    }
+
+    this.updateDays();
+  }
+
+  isLeapYear = (year: number) =>
+    (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+
+  getDaysInMonth(year: number, month: string): number {
+    switch (month) {
+      case '1': // January
+      case '3': // March
+      case '5': // May
+      case '7': // July
+      case '8': // August
+      case '10': // October
+      case '12': // December
+        return 31;
+      case '4': // April
+      case '6': // June
+      case '9': // September
+      case '11': // November
+        return 30;
+      case '2': // February
+        return this.isLeapYear(year) ? 29 : 28;
+      default:
+        throw new Error(`Invalid month: ${month}`);
+    }
+  }
+
+  updateDays() {
+    const daysInMonth = this.getDaysInMonth(
+      this.selectedYear,
+      this.selectedMonth!.toString()
+    );
+
+    this.days = [];
+    for (let day = 1; day <= daysInMonth; day++) {
+      const withinMinDate =
+        this.selectedYear > this.minDate.getFullYear() ||
+        (this.selectedYear == this.minDate.getFullYear() &&
+          this.selectedMonth > this.minDate.getMonth() + 1) ||
+        (this.selectedYear == this.minDate.getFullYear() &&
+          this.selectedMonth == this.minDate.getMonth() + 1 &&
+          day >= this.minDate.getDate());
+
+      const withinMaxDate =
+        this.selectedYear < this.maxDate.getFullYear() ||
+        (this.selectedYear == this.maxDate.getFullYear() &&
+          this.selectedMonth < this.maxDate.getMonth() + 1) ||
+        (this.selectedYear == this.maxDate.getFullYear() &&
+          this.selectedMonth == this.maxDate.getMonth() + 1 &&
+          day <= this.maxDate.getDate());
+
+      if (withinMinDate && withinMaxDate) {
+        this.days.push(day);
+      }
+    }
+
+    if (this.selectedDay == null) this.selectedDay = this.days[0];
+    if (this.selectedDay > daysInMonth) {
+      this.selectedDay = daysInMonth;
+    } else if (this.selectedDay < this.minDate.getDate()) {
+      this.selectedDay = this.minDate.getDate();
+    } else if (this.selectedDay > this.maxDate.getDate()) {
+      this.selectedDay = this.maxDate.getDate();
+    }
+  }
 }
